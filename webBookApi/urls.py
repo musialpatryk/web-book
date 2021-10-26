b823438a8bf73ce05556c6e97c0c72ff2f3014a4
PrzybylaM <mariusz.przybyla95@gmail.com>
2021-10-26 21:35:01 +0200

    REST

--- webBookApi/urls.py
+++ webBookApi/urls.py
@@ -1,4 +1,4 @@
-"""bookWebApi URL Configuration
+"""webBookApi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from webBookApi.api import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
