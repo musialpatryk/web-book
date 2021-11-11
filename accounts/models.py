c4ee9b69badbd31677af3a0140409cec1a46504c
NURSAA <63779425+NURSAA@users.noreply.github.com>
2021-11-11 13:42:07 +0100

    Add Login & Registration Logic Add Redirection & decorators

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django import forms
+from django.forms import ModelForm, PasswordInput
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your models here.
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Username...',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Email...',
+            })
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter Password...'})
+        self.fields['password2'].widget = PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Re-enter Password...'})
