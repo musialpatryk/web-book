9c133d3c265f2450e58a0c44d1d1180775bd21ec
ViciousCabaretV <mikolaj.porebski@gmail.com>
2021-11-14 16:00:19 +0100

    WB-21 pagination tool (#27)
    * #WB-21 pagination tool + authors list
    
    * #WB-21 cleaning up + filename change
    
    * #WB-21 constructor change and cosmetic changes + TODO note
    
    * #WB-21 pagination fix + merge conflicts
    
    Co-authored-by: Mikołaj Porębski <mikolaj.porebski@gamivo.com>

--- authors/views.py
+++ authors/views.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Author
+from helpers.pagination_tool import PaginationTool
+from django.http import HttpResponse
 
 @login_required(login_url='accounts:login')
-def author_list(request):
-    return render(request, 'authors/authors_list.html', {'authors': Author.objects.all()})
+def authors_list(request):
+    return render(request, 'authors/authors_list.html', {'authors': PaginationTool(Author(), request.GET.get('page'), request.GET.get('limit')).get_data()})
 
 @login_required(login_url='accounts:login')
 def author_details(request, slug):
     author = Author.objects.get(slug=slug)
-    # if null === book:
-    return render(request, 'authors/authors_details.html', {'author': author})
+    return render(request, 'authors/authors_details.html', {'author': Author.objects.get(id=id)})
 
---
+++ helpers/pagination_tool.py
@@ -0,0 +1,46 @@
+from global_settings import pagination_settings
+
+
+#TODO: wiecej informacji o bledach, na ktorych podstawie tylko bledne wartosci beda przywracane do domyslnych?
+class PaginationTool:
+    page = None
+    limit = None
+    object = None
+    offset = None
+
+    def __init__(self, object, page, limit):
+        self.object = object
+        self.page = page
+        self.limit = limit
+
+    def set_limit_and_offset(self):
+        self.page = int(self.page)
+        self.limit = int(self.limit)
+        offset = (self.page * self.limit) - self.limit
+        self.offset = offset
+        self.limit = offset + self.limit
+
+    def get_data(self):
+        self.prepare_data()
+        return self.object.__class__.objects.all()[self.offset:self.limit]
+
+    def prepare_data(self):
+        if not self.validate_args():
+            self.page = pagination_settings.default_pagination_page
+            self.limit = pagination_settings.default_pagination_limit
+        self.set_limit_and_offset()
+
+    def validate_args(self):
+        if self.page is None or self.limit is None:
+            return False
+
+        if self.page.isnumeric() is False or self.limit.isnumeric() is False:
+            return False
+
+        if int(self.page) <= 0 or int(self.limit) <= 0:
+            return False
+
+        if int(self.limit) not in pagination_settings.pagination_limits:
+            return False
+
+        return True
