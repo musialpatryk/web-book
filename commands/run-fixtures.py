d41e70009f4a3b921cf25bc482ed5b10c39d0de7
Patryk <musial.patryk10@gmail.com>
2021-11-22 20:35:46 +0100

    Fix system error in run-fixtures.py

--- commands/run-fixtures.py
+++ commands/run-fixtures.py
@@ -1,11 +1,6 @@
 import os
 
-def normalize(names):
-    return list(map(lambda name: name.rstrip(), names))
-
-
-lsStream = os.popen('ls fixtures/')
-fixturesNames = normalize(lsStream.readlines())
+fixturesNames = os.listdir('fixtures/')
 
 for name in fixturesNames:
     loadStream = os.popen('python manage.py loaddata --skip-checks fixtures/' + name)
