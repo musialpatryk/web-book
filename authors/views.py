0af94007596cecf0ad132b09466d0e193be8b589
NURSAA <63779425+NURSAA@users.noreply.github.com>
2021-11-28 16:10:54 +0100

    Modify default user permission

--- authors/views.py
+++ authors/views.py
@@ -2,12 +2,15 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Author
 from helpers.pagination_tool import PaginationTool
+from accounts.decorators import allowed_users, unauthenticated_user, admin_only
 
 @login_required(login_url='accounts:login')
+@allowed_users(allowed_roles=['viewer', 'admin'])
 def authors_list(request):
     offset, limit = PaginationTool(request.GET.get('page'), request.GET.get('limit')).get_data()
     return render(request, 'authors/authors_list.html', {'authors': Author.objects.all()[offset:limit]})
 
+@allowed_users(allowed_roles=['viewer', 'admin'])
 @login_required(login_url='accounts:login')
 def author_details(request, slug):
     author = Author.objects.get(slug=slug)
