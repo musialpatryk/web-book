955086e575f9684eb8acfc986a956705d4d17422
musialpatryk <musial.patryk10@gmail.com>
2021-12-22 21:05:33 +0100

    Changes: - author details - author images - book request list styling - move stars to seperate template

--- authors/models.py
+++ authors/models.py
@@ -1,5 +1,8 @@
+from django.core.validators import FileExtensionValidator
+
 from books.models import AbstractEntity
 from books.models import models
+from helpers.random_photo import get_random_photo
 
 
 class Author(AbstractEntity):
@@ -9,6 +12,11 @@ class Author(AbstractEntity):
     birthDate = models.DateField()
     slug = models.SlugField(default="None")
     rating = models.IntegerField(default=0)
+    image = models.ImageField(
+        default='default_author_1.jpg',
+        upload_to='author_images',
+        validators=[FileExtensionValidator(allowed_extensions=['png'])]
+    )
 
     def __str__(self):
         return self.name
